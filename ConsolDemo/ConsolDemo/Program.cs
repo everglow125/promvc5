77a0aa57dca693a08109910f95c2b4a653bc7257
everglow125 <183900112@qq.com>
2017-07-05 18:19:34 +0800

    debuggingdome

---
+++ ConsolDemo/ConsolDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Test();
+            } while (Console.ReadKey().ToString() == "Q");
+        }
+        static void Test()
+        {
+            try
+            {
+                bool result = false;
+                Thread unZipAndMatchThread = new Thread(() => ConvertInt("aaa"));
+                unZipAndMatchThread.IsBackground = true;
+                unZipAndMatchThread.Start();
+                result = true;
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static int ConvertInt(string aaa)
+        {
+            return Convert.ToInt32(aaa);
+        }
+    }
+}
